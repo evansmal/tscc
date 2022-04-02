bd43f262e5624a014bfe3006b2a47838621af56f
Evan Smal <evan.smal@hotmail.com>
2022-04-01 20:46:09 -0400

    Added test driver

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-node $SCRIPT_DIR/build/index.js $@
+cd $SCRIPT_DIR/write_a_c_compiler
+./test_compiler.sh ../../tscc $@
+
