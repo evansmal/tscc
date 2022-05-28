548079b69a9253edf0abb7d666765b085ae61844
Evan Smal <evan.smal@hotmail.com>
2022-05-28 11:45:02 -0400

    Updated the help message

--- src/index.ts
+++ src/index.ts
@@ -58,7 +58,7 @@ function run(input_filepath: string, output_filepath: string, opts: Options) {
 function main() {
     if (argv.length < 3) {
         console.log(`Got ${argv.length - 2} arguments but expected at least 1`);
-        console.log(`Usage: tscc <input_filepath> [--input] [--lex] [--ast] [--ir] [--asm]`);
+        console.log(`Usage: tscc <input_filepath> [--input] [--lex] [--ast] [--ir] [--asm] [--out]`);
         process.exit(0);
     }
     const input_filepath = argv[2];
