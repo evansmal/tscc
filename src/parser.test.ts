6a8d46bd915cba7cb8f67b5b0fa570ad2c6b33d1
Evan Smal <evan.smal@hotmail.com>
2023-02-19 10:56:17 -0500

    Upgrade to Node LTS and enable unit tests

---
+++ src/parser.test.ts
@@ -0,0 +1,9 @@
+import {} from "./parser.js";
+
+import test from "node:test";
+import assert from "node:assert";
+
+test("synchronous passing test", (t) => {
+    // This test passes because it does not throw an exception.
+    assert.strictEqual(1, 1);
+});
